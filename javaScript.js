b372767ba912b7fbc5ba7493cd0b4f8884943f2f
Abdul Samad Khan <abdulsamad31650@gmail.com>
2024-01-13 15:37:27 +0500

    Location fatched

--- javaScript.js
+++ javaScript.js
@@ -1,5 +1,8 @@
 // API KEY = 571edd05e907bc3296f22cb096132047
 
+const apiKey = '571edd05e907bc3296f22cb096132047';
+const apiUrl = 'https://api.openweathermap.org/data/2.5/weather';
+
 const form = document.querySelector('.form');
 // console.log(form);
 form.addEventListener('submit', function (e) {
@@ -12,4 +15,6 @@ form.addEventListener('submit', function (e) {
 
 function getWeather(location) {
 	console.log(`Your Location is: ${location}`);
+	let city = document.querySelector('.cityName');
+	city.innerHTML = location;
 }
