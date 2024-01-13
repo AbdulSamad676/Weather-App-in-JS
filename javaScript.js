a0a209109b7dc4eebeff560fcbc4d16eb5f8c287
Abdul Samad Khan <abdulsamad31650@gmail.com>
2024-01-13 15:33:52 +0500

    loaction fatched

--- javaScript.js
+++ javaScript.js
@@ -1,1 +1,15 @@
 // API KEY = 571edd05e907bc3296f22cb096132047
+
+const form = document.querySelector('.form');
+// console.log(form);
+form.addEventListener('submit', function (e) {
+	e.preventDefault();
+	let formData = new FormData(e.target);
+	let inputField = formData.get('inputText');
+	getWeather(inputField);
+	// console.log(inputField);
+});
+
+function getWeather(location) {
+	console.log(`Your Location is: ${location}`);
+}
