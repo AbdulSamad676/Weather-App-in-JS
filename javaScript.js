eaa42a1267542dc9ea10ff656e13f9c01efbb62a
Abdul Samad Khan <abdulsamad31650@gmail.com>
2024-01-13 15:49:03 +0500

    Api integrated

--- javaScript.js
+++ javaScript.js
@@ -1,6 +1,6 @@
 // API KEY = 571edd05e907bc3296f22cb096132047
 
-const apiKey = '571edd05e907bc3296f22cb096132047';
+const apiKey = '2888b5e36cc53f64de15e13abaefc2b8';
 const apiUrl = 'https://api.openweathermap.org/data/2.5/weather';
 
 const form = document.querySelector('.form');
@@ -14,7 +14,20 @@ form.addEventListener('submit', function (e) {
 });
 
 function getWeather(location) {
-	console.log(`Your Location is: ${location}`);
 	let city = document.querySelector('.cityName');
+	// console.log(`Your Location is: ${location}`);
+	const url = `${apiUrl}?q=${location}&appid=${apiKey}&units=metric`;
+
+	fetch(url)
+		.then(response => response.json())
+		.then(data => {
+			console.log(data);
+			city.textContent = data.name;
+			// temperatureElement.textContent = `${Math.round(data.main.temp)}°C`;
+			// descriptionElement.textContent = data.weather[0].description;
+		})
+		.catch(error => {
+			console.error('Error fetching weather data:', error);
+		});
 	city.innerHTML = location;
 }
